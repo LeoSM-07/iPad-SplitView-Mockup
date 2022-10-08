3ec8147789177fb8bfbe4418046e1b00f45701e9
LeoSM_07 <leosmindlin@icloud.com>
2022-10-08 08:52:13 -0700

    Initial Commit

---
+++ iPadSplitViewTesting/iPadSplitViewTestingApp.swift
@@ -0,0 +1,17 @@
+//
+// iPadSplitViewTestingApp.swift
+// iPadSplitViewTesting
+//
+// Created by LeoSM_07 on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct iPadSplitViewTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
